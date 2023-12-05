0c20a291cbccd2fd3390bc29a540068b20e37366
KimAeris <kar2125@naver.com>
2023-12-05 16:12:58 +0900

    TodoInput

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TodoHeader />
+  <TodoInput />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoHeader from "./components/TodoHeader.vue";
+import TodoInput from "./components/TodoInput.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    TodoHeader,
+    TodoInput,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>TODO APP</h1>
+</template>
+
+<script>
+export default {};
+</script>
